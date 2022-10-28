07422d3b09d3741246c6af156d8b4ca5c888333c
aleroom <aleroom@yahoo.com>
2022-10-28 15:58:12 +0300

    modified style

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-const data = new Date();
-const day = data.getDate();
-const month = data.getMonth() + 1;
-const year = data.getFullYear();
-
-const $CARENT_DATA = document.querySelector(".current_data");
-$CARENT_DATA.textContent = `${day}.${month}.${year}.`;
